f5a15e10bbcf53e80205825b6c1e967cac67a7b3
benxiao <ben060788@gmail.com>
2018-12-06 02:30:44 +0800

    index_name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 起手式
+                    Laravel 教學/測試
                 </div>
 
                 <div class="links">
