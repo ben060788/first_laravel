1fbb1d293729c233e6ab729b2010b76a5c7b6caf
benxiao <ben060788@gmail.com>
2018-12-10 15:06:25 +0800

    Add front-end layout, view,bootstrap

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,30 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//laravel預設首頁
 Route::get('/', function () {
     return view('welcome');
 });
 
+//顯示phpinfo()
 Route::get('/index', function () {
-    return view('index');
+    return view('frontend.index');
 });
+//參數傳遞
+Route::get('/param', function () {
+    return view('frontend.hello', ['name' => 'Ben', 'years' => '23']);
+});
+
+//bootstrap index2
+Route::get('/index2', function () {
+    return view('frontend.index2');
+})->name('home');
+Route::get('/about', function () {
+    return view('frontend.about');
+})->name('about');
+Route::get('/product', function () {
+    return view('frontend.product');
+})->name('product');
+Route::get('/store', function () {
+    return view('frontend.store');
+})->name('store');
